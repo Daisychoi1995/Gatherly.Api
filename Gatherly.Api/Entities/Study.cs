481e609bf869b9f21634b03f2ed113c6ce94fd77
Daisy Choi <chj15937@gmail.com>
2025-05-02 12:25:39 +1200

    added Entities for study and user

---
+++ Gatherly.Api/Entities/Study.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Gatherly.Api.Entities;
+
+public class Study
+{
+  public int Id { get; set; }
+
+  public required string Title { get; set; }
+
+  public required string Description { get; set; }
+
+  public required List<string> Category { get; set; }
+
+  public required int MaxMember { get; set; }
+
+  public required string Place { get; set; }
+
+  public required int CreatorId { get; set; }
+
+  public required DateTime StudyDate { get; set; }
+
+  public virtual User? UserName { get; set; }
+}
---
+++ Gatherly.Api/Entities/User.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Gatherly.Api.Entities;
+
+public class User
+{
+  public int Id { get; set; }
+
+  public required string UserName { get; set; }
+
+  public required string FullName { get; set; }
+
+  public required string Description { get; set; }
+
+  public required string Email { get; set; }
+
+  public required string PasswordHash { get; set; }
+
+  public required string ProfileUrl { get; set; }
+
+  public IReadOnlyList<Study>? StudyPost { get; set; }
+
+}
